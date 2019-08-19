b9b7dd3e98b09ec076ca8f69ae1688bf2e16a5f3
gminova <minova1995@gmail.com>
2019-08-19 14:26:29 +0100

    tape and db

--- server/tests/routes.test.js
+++ server/tests/routes.test.js
@@ -1,5 +1,19 @@
-const test = require('tape');
-const request = require('supertest');
-const app = require('../server');
+const test = require("tape");
+const request = require("supertest");
+const app = require("../server");
 
 //Fill this with many many tests YAY!! 😜😩
+test("Check tape is working", t => {
+  t.equal(1, 1, "One should be one");
+  t.end();
+});
+
+test("Check root / route is being served", t => {
+  request(app)
+    .get("/facsters")
+    .end((err, res) => {
+      t.error(err);
+      t.equal(res.statusCode, 200, "Home should return status code 200");
+      t.end();
+    });
+});
